528c7d88d83b4b3bea08ea378e104542c3dc0063
MattGr$4dev <mattgr34dev@gmail.com>
2024-10-15 16:34:12 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+x_version 'cerulean'
+game 'gta5' 
+
+author 'Glorys Project'
+description 'Traffic Advisor for HWY'
+version '1.0.0'
+
+-- What to run
+client_script 'function.lua'
+server_script 'commands.lua'
